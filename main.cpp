5debb9d868efd9217a6e24681a998d9237297519
JuliaLi05 <jula.litawa@gmail.com>
2026-05-26 14:04:38 +0200

    Fix formatting and clean up main.cpp

--- main.cpp
+++ main.cpp
@@ -34,5 +34,3 @@ int main() {
     }
     return 0;
 }
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
